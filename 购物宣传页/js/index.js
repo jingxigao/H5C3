a3980d3cceecb5200cebf418d2c6c212119041c0
JingxiGao <jingxi.gao@tum.de>
2018-11-20 11:52:51 +0100

    11.20

---
+++ 购物宣传页/js/index.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.container').fullpage({
+        sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+        verticalCentered:false,
+        navigation:true
+    });
+
+});
